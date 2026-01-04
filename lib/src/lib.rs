93eb404cd03490fc377a6c29cdcfe10ff8c5c4ea
aerosouund <aerosound161@gmail.com>
2026-01-04 23:42:55 +0200

    relocate handle ethernet function
    Signed-off-by: aerosouund <aerosound161@gmail.com>

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,6 +1,7 @@
 use libc::in_addr;
 use log::{debug, error, info};
 use mio::net::UnixStream;
+use muxer::StreamConnCtx;
 use pnet::packet::arp::ArpOperation;
 use pnet::packet::arp::MutableArpPacket;
 use pnet::packet::ethernet::EtherTypes::Arp;
@@ -8,7 +9,7 @@ use pnet::packet::ethernet::EthernetPacket;
 use pnet::packet::ipv4::Ipv4;
 use pnet::packet::Packet;
 use std::io;
-use std::io::Write;
+use std::io::{Read, Write};
 use std::os::fd::RawFd;
 
 pub const MAX_FRAME: usize = 65535 + 4;
@@ -137,4 +138,43 @@ pub unsafe fn setup_sig_handler(handler: usize, signal: libc::c_int) {
 
     libc::sigemptyset(&mut sa.sa_mask as *mut libc::sigset_t);
     libc::sigaction(signal, &sa, std::ptr::null_mut());
+}
+
+pub fn handle_tap_ethernet(
+    ctx: &mut StreamConnCtx,
+) -> Result<Vec<EthernetPacket<'static>>, io::Error> {
+    let mut buf = [0u8; MAX_FRAME];
+    let mut v4_packets: Vec<EthernetPacket<'static>> = Vec::new();
+    let mut offset = 0;
+    if ctx.partial_frame.len() > 0 {
+        buf.clone_from_slice(ctx.partial_frame());
+        offset += ctx.partial_frame.len();
+    }
+    let mut n = ctx.stream.read(&mut buf[offset..])?;
+
+    while n > 4 {
+        let packet_size: [u8; 8] = buf[offset..offset + 4].try_into().map_err(|e| {
+            error!("failed to parse packet size {e}");
+            io::Error::new(io::ErrorKind::InvalidData, "failed to parse packet size")
+        })?;
+        let l2len = usize::from_be_bytes(packet_size);
+        if l2len > MAX_FRAME {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "Frame too large",
+            ));
+        }
+        offset += 4;
+        n -= 4;
+        if let Some(packet) = EthernetPacket::owned(buf[offset..offset + l2len].to_vec()).take() {
+            v4_packets.push(packet);
+        };
+
+        offset += l2len;
+        n -= l2len;
+        if l2len > buf[offset..].len() {
+            ctx.partial_frame.clone_from_slice(&buf[offset..]);
+        }
+    }
+    Ok(v4_packets)
 }
--- passt/src/main.rs
+++ passt/src/main.rs
@@ -1,12 +1,10 @@
 #![allow(non_upper_case_globals)]
 use clap::Parser;
 use libpasst::muxer::{ConnEnum, Muxer, StreamConnCtx};
-use libpasst::{exit_handler, handle_packets, setup_sig_handler, MAX_FRAME};
+use libpasst::{exit_handler, handle_packets, handle_tap_ethernet, setup_sig_handler};
 use log::{debug, error, info};
 use mio::net::UnixListener;
 use mio::{Events, Interest, Poll, Token};
-use pnet::packet::ethernet::EthernetPacket;
-use std::io::Read;
 
 use std::io::{self};
 use std::os::unix::io::AsRawFd;
@@ -71,43 +69,5 @@ fn main() -> io::Result<()> {
                 _ => {}
             }
         }
-        // find a way to drop tx if the function returns an error
     }
-}
-
-fn handle_tap_ethernet(ctx: &mut StreamConnCtx) -> Result<Vec<EthernetPacket<'static>>, io::Error> {
-    let mut buf = [0u8; MAX_FRAME];
-    let mut v4_packets: Vec<EthernetPacket<'static>> = Vec::new();
-    let mut offset = 0;
-    if ctx.partial_frame.len() > 0 {
-        buf.clone_from_slice(ctx.partial_frame());
-        offset += ctx.partial_frame.len();
-    }
-    let mut n = ctx.stream.read(&mut buf[offset..])?;
-
-    while n > 4 {
-        let packet_size: [u8; 8] = buf[offset..offset + 4].try_into().map_err(|e| {
-            error!("failed to parse packet size {e}");
-            io::Error::new(io::ErrorKind::InvalidData, "failed to parse packet size")
-        })?;
-        let l2len = usize::from_be_bytes(packet_size);
-        if l2len > MAX_FRAME {
-            return Err(io::Error::new(
-                io::ErrorKind::InvalidData,
-                "Frame too large",
-            ));
-        }
-        offset += 4;
-        n -= 4;
-        if let Some(packet) = EthernetPacket::owned(buf[offset..offset + l2len].to_vec()).take() {
-            v4_packets.push(packet);
-        };
-
-        offset += l2len;
-        n -= l2len;
-        if l2len > buf[offset..].len() {
-            ctx.partial_frame.clone_from_slice(&buf[offset..]);
-        }
-    }
-    Ok(v4_packets)
 }
