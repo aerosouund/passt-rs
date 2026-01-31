022c9183a8f4060a93c5a2f8f49abe335e603535
aerosouund <aerosound161@gmail.com>
2026-01-31 21:59:25 +0200

    use the same names in the ip config as the c version
    Signed-off-by: aerosouund <aerosound161@gmail.com>

--- passt/src/conf.rs
+++ passt/src/conf.rs
@@ -1,7 +1,12 @@
+use libpasst::conf::Mode;
+
 #[derive(Parser, Debug)]
 #[command(author, version, about)]
 struct Args {
     /// Socket path for Unix domain socket
     #[arg(short = 's', long = "socket-path", default_value = "/tmp/my_socket")]
     socket_path: String,
+    /// Passt mode
+    #[arg(short = 'm', long = "mode", default_value = Mode::Passt)]
+    socket_path: Mode,
 }
