b8f989ea6bf593363b7791fbeb7a338972988970
aerosouund <aerosound161@gmail.com>
2026-04-29 21:32:46 +0000

    make that damn mistake
    Signed-off-by: aerosouund <aerosound161@gmail.com>

--- lib/src/netlink.rs
+++ lib/src/netlink.rs
@@ -292,12 +292,9 @@ pub fn nl_get_default_gw(
         if let NlTypeWrapper::Rtm(_) = rtm.nl_type()
             && let Some(payload) = rtm.get_payload()
         {
-            // ammar: this conditional still applies though, dont see how we had to omit it
-            // the core of the problem is that the c code gets a gateway attribute but the
-            // rust does not
-            // if *payload.rtm_dst_len() != 0 {
-            //     continue;
-            // }
+            if *payload.rtm_dst_len() != 0 {
+                continue;
+            }
 
             for attr in payload.rtattrs().iter() {
                 eprintln!(
